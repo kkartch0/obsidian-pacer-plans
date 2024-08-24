355107ee0191a362b5d87b9fdfb63ba8d1039e93
Ky Kartchner <kartchnerky@gmail.com>
2024-08-24 13:15:33 -0600

    clean up, update so new plan file is opened

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { Editor, MarkdownView, Notice, Plugin } from 'obsidian';
 import { PacerPlanSettingsTab } from './PacerPlanSettingsTab';
 import { PacerPlanEditCreateModal } from './PacerPlanEditCreateModal';
+import { executionAsyncId } from 'async_hooks';
 
 // Remember to rename these classes and interfaces!
 
@@ -18,24 +19,12 @@ export default class PacerPlansPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
 			id: 'create-new-pacer-plan',
 			name: 'Create new Pacer Plan',
 			callback: () => {
-				new PacerPlanEditCreateModal(this.app, (result) => {
+				new PacerPlanEditCreateModal(this.app, async (result) => {
 					result.generateTasks();
 
 					let resultString = result.toString();
@@ -43,31 +32,18 @@ export default class PacerPlansPlugin extends Plugin {
 					console.log(resultString);
 
 					// write string to new file using vault
-					this.app.vault.create(result.title + ".md", resultString);
+					const planFile = await this.app.vault.create(result.title + ".md", resultString);
+
+					// open the newly created file
+					this.app.workspace.getLeaf("tab").openFile(planFile);
+					
 				}).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
 			}
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new PacerPlanSettingsTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
