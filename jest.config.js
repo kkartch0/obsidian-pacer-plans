651df83cdec6fa7d2bd2cc3166d4a8c68e376d97
Ky Kartchner <kartchnerky@gmail.com>
2024-09-19 16:39:33 -0600

    fix issues with test

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,13 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'jsdom',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      tsconfig: 'tsconfig.json',
+    }]
+  },
+  moduleNameMapper: {
+    'pluralize': '<rootDir>/node_modules/pluralize/pluralize.js',
+  },
 };
